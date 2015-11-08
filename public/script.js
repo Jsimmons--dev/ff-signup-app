24732783665dff120aba68c8ddea822abb55accc
jsimmons--dev <jsimmons@udel.edu>
2015-11-07 22:12:31 -0500

    added style

--- public/script.js
+++ public/script.js
@@ -1,4 +1,6 @@
 function submit(){
+	var form = document.getElementById("subForm");
+	var thank = document.getElementById("thank");
 	$.ajax({
 		url: 'http://api.joshsimmons.rocks/freefall/maillist',
 		type: 'POST',
@@ -6,6 +8,13 @@ function submit(){
 			email: $("#email").val() 
 		},
 		success: function(res){
+				form.style.display = "none"
+				thank.style.display = "block"
+				setTimeout(function(){
+				form.style.display = "block";
+				thank.style.display = "none";
+				$("#email").val("");
+				},5000);
 		},
 		error: function(XMLHttpRequest,textStatus,errorThrown){
 			console.log(textStatus);
