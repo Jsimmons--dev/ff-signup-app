ca5afbfd3b41068848c667a6a97f2956dc9f270e
jsimmons--dev <jsimmons@udel.edu>
2015-11-07 20:14:11 -0500

    write to db

--- public/script.js
+++ public/script.js
@@ -3,9 +3,14 @@ function submit(){
 	$.ajax({
 			url: 'http://api.joshsimmons.rocks/freefall/maillist',
 		type: 'POST',
-		data: {email:$("#email").text()},
+		dataType: 'json',
+		data: {
+			"email": $("#email").val()
+		},
 		success: function(res){
-			console.log(res);
+		},
+		error: function(XMLHttpRequest,textStatus,errorThrown){
+			console.log(textStatus);
 		}
 	});
 }
