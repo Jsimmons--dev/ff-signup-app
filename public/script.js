3a7a967efe1f66c3f9636f023dd5395d0af1319e
Josh <jsimmons@udel.edu>
2015-11-08 01:32:47 +0000

    Revert "write to db"
    This reverts commit ca5afbfd3b41068848c667a6a97f2956dc9f270e.

--- public/script.js
+++ public/script.js
@@ -3,14 +3,9 @@ function submit(){
 	$.ajax({
 			url: 'http://api.joshsimmons.rocks/freefall/maillist',
 		type: 'POST',
-		dataType: 'json',
-		data: {
-			"email": $("#email").val()
-		},
+		data: {email:$("#email").text()},
 		success: function(res){
-		},
-		error: function(XMLHttpRequest,textStatus,errorThrown){
-			console.log(textStatus);
+			console.log(res);
 		}
 	});
 }
